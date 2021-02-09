8422d31eea5d7c34bae731e2d1d3bcbffb6d48b3
atrox3d <atrox3d@gmail.com>
2021-02-09 10:50:25 +0100

    use turtle

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+my_screen = Screen()
+
+print(my_screen.canvheight)
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+my_screen.exitonclick()
+
+
