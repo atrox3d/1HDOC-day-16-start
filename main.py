79d7a9d684ad09e58dcc41db13cb9f06bc0b17ff
atrox3d <atrox3d@gmail.com>
2021-02-09 11:18:17 +0100

    add prettytable

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# my_screen = Screen()
+#
+# print(my_screen.canvheight)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+# my_screen.exitonclick()
+#
+#
+from prettytable import PrettyTable
 
-timmy = Turtle()
-my_screen = Screen()
+table = PrettyTable()
+table.add_column("Pokemon name", ["Picachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.add_column("Test", [1, 2, 3])
 
-print(my_screen.canvheight)
-timmy.shape("turtle")
-timmy.color("red")
-timmy.forward(100)
-my_screen.exitonclick()
+print(table.align)
+table.align = "l"
+
+print(table)
+
 
 
